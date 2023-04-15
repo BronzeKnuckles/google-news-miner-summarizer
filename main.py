fc5fcd106c1671c733f2dd9ee5438fcaea351b1c
BronzeKnuckles <srivenkatesh1999@gmail.com>
2023-04-15 12:39:04 -0400

    init

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
+from newspaper import Article
 from gnews import GNews
+
 
 google_news = GNews()
 
+google_news.period = "7d"  # News from last 7 days
+google_news.max_results = 10  # number of responses across a keyword
+# google_news.country = "United States"  # News from a specific country
+google_news.language = "english"  # News in a specific language
+# google_news.exclude_websites = []
+# google_news.start_date = (2020, 1, 1)  # Search from 1st Jan 2020
+# google_news.end_date = (2020, 3, 1)  # Search until 1st March 2020
 
-google_news.start_date = (2021, 1, 1)
-google_news.end_date = (2021, 2, 1)
-google_news.max_results = 2
+# result = google_news.get_news('"Citadel-securities"')
 
-result = google_news.get_news('"WORLD"')
-print(result)
 
-print(google_news.get_news_by_topic.__doc__)
+json_resp = google_news.get_news("Scotia Bank")
 
-google_news.get_news_by_location("WORLD")
+with open("newsMined.txt", "w") as f:
+    for i in range(10):
+        link = json_resp[i]["url"]
+        try:
+            article = Article(link)
+            article.download()
+            article.parse()
+            article.nlp()
+            print(
+                f"\nArticle Title: \t{article.title}\nArticle Keywords: \t{article.keywords}\nArticle Summary: \n{article.summary}\n\n\n"
+            )
+            f.write(
+                f"\nArticle Title: \t{article.title}\nArticle Keywords: \t{article.keywords}\nArticle Summary: \n{article.summary}\n\n\n"
+            )
+        except:
+            print(f"ERROR!!!! for link: {link}\n")
+            f.write(f"ERROR!!!! for link: {link}\n")
 
-result = google_news.get_news('"WORLD"')
-print(result)
+
+print("Task Done")
